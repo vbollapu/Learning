2a00cb9a584856577bf6e084c6735ff4427ccd3c
vbollapu <bollapuvinay43@gmail.com>
2017-12-27 14:05:55 +0530

    Initial commit

---
+++ MVCgit/MVCgit/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MVCgit.MvcApplication" Language="C#" %>
---
+++ MVCgit/MVCgit/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MVCgit.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductId { get; set; }
+
+        [MaxLength(15)]
+        public string ProductName { get; set; }
+    }
+}
